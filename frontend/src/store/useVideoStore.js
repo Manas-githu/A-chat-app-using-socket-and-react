817928e8db98331447904e69b2a212e1f2363c86
Manas_on_this_terminal <93460598+Manas-githu@users.noreply.github.com>
2025-02-14 12:42:27 +0530

    Update useVideoStore.js back to meetered ca but its working on another site so lets see

--- frontend/src/store/useVideoStore.js
+++ frontend/src/store/useVideoStore.js
@@ -25,31 +25,30 @@ import { useChatStore } from "./useChatStore";
 // };
 
 const iceServers = {
-iceServers :  [
-  { urls: 'stun:stun.relay.metered.ca:80' },
-  {
-    urls: 'turn:global.relay.metered.ca:80',
-    username: 'c6423e269f600466978874b9',
-    credential: 'WHntcvF0TEmGcm8h'
-  },
-  {
-    urls: 'turn:global.relay.metered.ca:80?transport=tcp',
-    username: 'c6423e269f600466978874b9',
-    credential: 'WHntcvF0TEmGcm8h'
-  },
-  {
-    urls: 'turn:global.relay.metered.ca:443',
-    username: 'c6423e269f600466978874b9',
-    credential: 'WHntcvF0TEmGcm8h'
-  },
-  {
-    urls: 'turns:global.relay.metered.ca:443?transport=tcp',
-    username: 'c6423e269f600466978874b9',
-    credential: 'WHntcvF0TEmGcm8h'
-  }
-]
+  iceServers: [
+    { urls: "stun:stun.relay.metered.ca:80" },
+    { 
+      urls: "turn:global.relay.metered.ca:80", 
+      username: "c6423e269f600466978874b9", 
+      credential: "WHntcvF0TEmGcm8h" 
+    },
+    { 
+      urls: "turn:global.relay.metered.ca:80?transport=tcp", 
+      username: "c6423e269f600466978874b9", 
+      credential: "WHntcvF0TEmGcm8h" 
+    },
+    { 
+      urls: "turn:global.relay.metered.ca:443", 
+      username: "c6423e269f600466978874b9", 
+      credential: "WHntcvF0TEmGcm8h" 
+    },
+    { 
+      urls: "turns:global.relay.metered.ca:443?transport=tcp", 
+      username: "c6423e269f600466978874b9", 
+      credential: "WHntcvF0TEmGcm8h" 
+    }
+  ]
 };
-
 
 
 
